f25a4802baf3ee77569837909e258021b1717341
Elias Gerster <elias.gerster@aol.de>
2024-04-26 11:06:09 +0200

    added step7

---
+++ session02/step7/minmax.cpp
@@ -0,0 +1,81 @@
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <random>
+#include <vector>
+
+void rand(std::size_t n, double* x) {
+  #pragma omp parallel
+  {
+    auto seed = std::random_device()();
+    thread_local std::mt19937 mt{seed};
+    std::uniform_real_distribution<double> uniform(-1, 1);
+
+    #pragma omp for
+    for (std::size_t i = 0; i < n; ++i) {
+      x[i] = uniform(mt);
+    }
+  }
+}
+
+std::pair<double, double> minmax(std::size_t n, const double* x) {
+  double min_value = std::numeric_limits<double>::max();
+  double max_value = std::numeric_limits<double>::min();
+  #pragma omp parallel for reduction(min:min_value) reduction(max:max_value)
+  for (std::size_t i = 0; i < n; ++i) {
+    if (x[i] < min_value) {
+      min_value = x[i];
+    }
+    if (x[i] > max_value) {
+      max_value = x[i];
+    }
+  }
+  return {min_value, max_value};
+}
+
+auto now() {
+  return std::chrono::high_resolution_clock::now();
+}
+auto elapsed_time_in_seconds(auto t0, auto t1) {
+  using namespace std::chrono;
+  return duration<double, seconds::period>(t1 - t0).count();
+}
+
+int main(int argc, char* argv[]) {
+  const char* cmdname = *argv++;
+  --argc;
+  std::size_t len = 1 << 16;
+
+  if (argc > 0 && **argv) {
+    char* endptr;
+    len = std::strtoull(*argv, &endptr, 0);
+    if (len == 0 || *endptr) {
+      std::cerr << cmdname << ": invalid length: " << *argv << std::endl;
+      std::exit(1);
+    }
+    ++argv;
+    --argc;
+  }
+
+  if (argc > 0) {
+    std::cerr << "Usage: " << cmdname << " length" << std::endl;
+    std::exit(1);
+  }
+
+  auto t0 = now();
+  std::vector<double> x(len);
+  auto t1 = now();
+  rand(len, &x[0]);
+
+  auto t2 = now();
+  auto [min_value, max_value] = minmax(len, &x[0]);
+  auto t3 = now();
+
+  std::cout << "minimal value: " << min_value << std::endl;
+  std::cout << "maximal value: " << max_value << std::endl;
+
+  std::cout << "time required for vector construction: " << elapsed_time_in_seconds(t0, t1) << " s"  << std::endl;
+  std::cout << "time required for initialization:      " << elapsed_time_in_seconds(t1, t2) << " s"  << std::endl;
+  std::cout << "time required for minmax:              " << elapsed_time_in_seconds(t2, t3) << " s"  << std::endl;
+}
