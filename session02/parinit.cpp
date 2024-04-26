3a7de0a1d39b47e7f29f98df199f1580ddb0b6c9
Elias Gerster <74102959+pedeEli@users.noreply.github.com>
2024-04-26 08:39:49 +0200

    added timeing functions to parinit.cpp

--- session02/parinit.cpp
+++ session02/parinit.cpp
@@ -1,6 +1,17 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <chrono>
+
+
+auto now() {
+	return std::chrono::high_resolution_clock::now();
+}
+
+auto elapsed_time_in_seconds(auto t0, auto t1) {
+	using namespace std::chrono;
+	return duration<double, seconds::period>(t1 - t0).count();
+}
 
 void init(std::size_t n, double* x, double val) {
 	#pragma omp parallel for
@@ -32,16 +43,23 @@ int main(int argc, char* argv[]) {
 		}
 		++argv; --argc;
 	}
-	
+
 	if (argc > 0) {
 		std::cerr << "Usage: " << cmdname << " length" << std::endl;
 		std::exit(1);
 	}
 
+	auto t0 = now();
 	std::vector<double> x(len);
-	init(len, &x[0], 42);
 	std::vector<double> y(len);
+	init(len, &x[0], 42);
 	init(len, &y[0], 7);
+
+	auto t1 = now();
 	axpy(len, 2.0, &x[0], &y[0]);
+	auto t2 = now();
+
 	std::cout << y[len-1] << std::endl;
+	std::cout << "time required for initialization: " << elapsed_time_in_seconds(t0, t1) << "s" << std::endl;
+	std::cout << "time required for axpy: " << elapsed_time_in_seconds(t1, t2) << "s" << std::endl;
 }
